d8e62ad844932d314d828d19bf5530684485d7a2
Shahid Adoni <shahid.adoni@corp.rapyder.com>
2022-12-19 19:40:32 +0530

    added index.html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!!!");
+import express from "express";
+import path from "path";
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, './public/')))
+
+app.get("/health-check", (_, res) => {
+    res.send("Hello world!!!!")
+})
+
+app.listen(3000, () => {
+    console.log("Server running at http://localhost:3000")
+})
---
+++ src/public/script.js
@@ -0,0 +1,8 @@
+const msgElement = document.getElementById("messages");
+const formElement = document.getElementById("submit-form");
+
+formElement.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const text = submit-form.value
+    console.log(text)
+})
